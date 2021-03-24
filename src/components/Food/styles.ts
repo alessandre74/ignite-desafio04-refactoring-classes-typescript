c60a3a45e9f134f36b058d36ccd1409b95a4b107
Alessandre Livramento <aleliv74@gmail.com>
2021-03-24 11:44:18 -0300

    Refactoring de classes e typescript

--- src/components/Food/styles.ts
+++ src/components/Food/styles.ts
@@ -1,6 +1,9 @@
-import styled, { css } from 'styled-components';
+import styled, { css } from 'styled-components'
+interface ContainerProps {
+  available: boolean
+}
 
-export const Container = styled.div`
+export const Container = styled.div<ContainerProps>`
   background: #f0f0f5;
   border-radius: 8px;
 
@@ -12,9 +15,9 @@ export const Container = styled.div`
     transition: 0.3s opacity;
     text-align: center;
 
-    ${props =>
-    !props.available &&
-    css`
+    ${(props) =>
+      !props.available &&
+      css`
         opacity: 0.3;
       `};
 
@@ -142,4 +145,4 @@ export const Container = styled.div`
       }
     }
   }
-`;
+`
